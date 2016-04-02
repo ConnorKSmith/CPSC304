0afbfbbaf111ad14b8a7b5ee4204219e1804549a
mrjko <mr_jko@hotmail.com>
2016-04-02 13:30:10 -0700

    finished all the frontend, ensure populatingDB.sql runs with one click

--- populatingDB.sql
+++ populatingDB.sql
@@ -1,6 +1,5 @@
-DROPPING ALL THE TABLES OVER FOREIGN KEY CONSTRAINTS:
-
 SET FOREIGN_KEY_CHECKS = 0;
+
 Drop table if exists Account;
 Drop table if exists Achievement;
 Drop table if exists Admin;
@@ -16,9 +15,130 @@ Drop table if exists OwnsGame;
 Drop table if exists Player;
 Drop table if exists Review;
 Drop table if exists WithinGroup;
+
 SET FOREIGN_KEY_CHECKS = 1;
 
-INSERTING INTO TABLES
+create table Account (
+	userID integer auto_increment,
+	userName varchar(30) not null unique,
+	password varchar(30) not null,
+	description varchar(400),
+	primary key(userID),
+    check (description<=400)
+);
+
+create table FriendsWith (
+	userID1 integer,
+	userID2 integer,
+	foreign key(userID1) references Account(userID) on delete cascade on update cascade,
+	foreign key(userID2) references Account(userID) on delete cascade on update cascade
+);
+
+create table FriendGroup(
+    gID integer auto_increment,
+    groupName varchar(30) not null unique,
+    groupDesc varchar(400),
+    creatorUserID integer,
+    dateCreated Date not null,
+    primary key(gID),
+    foreign key(creatorUserID) references Account(userID) on delete cascade on update cascade 
+);
+
+create table WithinGroup(
+    memberUserID integer,
+    withinGroupID integer,
+    foreign key(memberUserID) references Account(userID) on delete cascade on update cascade,
+    foreign key(withinGroupID) references FriendGroup(gID) on delete cascade on update cascade
+);
+
+create table Player(
+    playerID integer,
+    primary key(playerID),
+    foreign key(playerID) references Account(userID) on delete cascade on update cascade
+);
+
+create table Developer(
+    developerID integer,
+    primary key(developerID),
+    foreign key(developerID) references Account(userID) on delete cascade on update cascade
+);
+
+create table Admin(
+    adminID integer,
+    primary key(adminID),
+    foreign key(adminID) references Account(userID) on delete cascade on update cascade
+);
+
+create table Game (
+    gameID integer auto_increment,
+    gName varchar(30) not null unique,
+    gDescription varchar(1000),
+    creatorID integer,
+    currentPrice integer not null,
+    primary key(gameID),
+    foreign key(creatorID) references Developer(developerID) on delete cascade on update cascade
+);
+
+create table Review (
+    reviewID integer auto_increment,
+    reviewDesc varchar(400) not null,
+    rating integer not null,
+    datePosted Date not null,
+    reviewerID integer,
+    gameReviewedID integer,
+    primary key(reviewID),
+    foreign key(reviewerID) references Account(userID) on delete cascade on update cascade,
+    foreign key(gameReviewedID) references Game(gameID) on delete cascade on update cascade
+);
+
+create table Genre (
+    genre varchar(30) unique,
+    primary key(genre)
+);
+
+create table OwnsGame (
+    ownerID integer,
+    gameID integer,
+    playHours integer not null default 0,
+    priceBoughtAt integer not null,
+    foreign key(ownerID) references Player(playerID) on delete cascade on update cascade,
+    foreign key(gameID) references Game(gameID) on delete cascade on update cascade
+);
+
+create table HasGenre (
+    gameID integer,
+    genre varchar(30),
+    foreign key(gameID) references Game(gameID) on delete cascade on update cascade,
+    foreign key(genre) references Genre(genre) on delete cascade on update cascade
+);
+
+create table Achievement (
+    aID integer auto_increment,
+    aDesc varchar(200),
+    aName varchar(30) not null,
+    totalNeeded integer not null,
+    gameID integer,
+    foreign key (gameID) references Game(gameID) on delete cascade on update cascade,
+    primary key(aID)
+);
+
+create table HasWorkTowards (
+    playerID integer,
+    aID integer,
+    progress integer not null,
+    isComplete Boolean default false,
+    foreign key (playerID) references Player(playerID) on delete cascade on update cascade,
+    foreign key (aID) references Achievement(aID) on delete cascade on update cascade
+);
+
+create table InAchievement (
+    playerID integer,
+    aID integer,
+    gameID integer,
+    foreign key(playerID) references Player(playerID) on update cascade on delete cascade,
+    foreign key(aID) references Achievement(aID) on update cascade on delete cascade,
+    foreign key(gameID) references Game(gameID) on update cascade on delete cascade
+);
 
 insert into Genre values ("Action");
 insert into Genre values ("Adventure");
@@ -32,16 +152,16 @@ insert into Genre values ("Sports");
 insert into Genre values ("Strategy");
 insert into Genre values ("Horror");
 
-insert into Account values(1,"player1","1234","player");
-insert into Account values(2,"player2","1234","player");
-insert into Account values(3,"player3","1234","player");
-insert into Account values(4,"player4","1234","player");
-insert into Account values(5,"player5","1234","player");
-insert into Account values(6,"dev1","1234","developer");
-insert into Account values(7,"dev2","1234","developer");
-insert into Account values(8,"dev3","1234","developer");
-insert into Account values(9,"dev4","1234","developer");
-insert into Account values(10,"dev5","1234","developer");
+insert into Account values(1,"jimmy","1234","player");
+insert into Account values(2,"jin","1234","player");
+insert into Account values(3,"connor","1234","player");
+insert into Account values(4,"thomas","1234","player");
+insert into Account values(5,"carlos","1234","player");
+insert into Account values(6,"tom","1234","developer");
+insert into Account values(7,"jerry","1234","developer");
+insert into Account values(8,"tiffany","1234","developer");
+insert into Account values(9,"christina","1234","developer");
+insert into Account values(10,"joy","1234","developer");
 insert into Account values(11,"admin","1234","admin");
 
 insert into Player values(1);
@@ -115,6 +235,4 @@ insert into InAchievement values(2,2,2);
 insert into InAchievement values(3,3,3);
 insert into InAchievement values(4,4,4);
 insert into InAchievement values(5,5,5);
-
-
 
